e14bf4e96965e5c539d7739a1dc40ae03601ae1d
Spyke27 <rodrigoeufrasio2@gmail.com>
2022-11-22 12:14:32 -0300

    projeto filmes em cartaz

---
+++ README.md
@@ -0,0 +1,13 @@
+<h1 align="center">Filmes em Cartaz</h1>
+
+## Principais Tecnologias Usadas 📓
+<ul>
+    <li>HTML</li>
+    <li>CSS</li>
+    <li>JavaScript</li>
+    <li>Bootstrap</li>    
+</ul>
+
+## Projeto 💻
+ Este projeto foi feito em sala de aula pela Generation, com o intuito de treinar e aperfeiçoar o uso de bootstrap. Nele foi utilizado a API da b7web fornecida em suas aulas no curso.
+
---
+++ assets/js/script.js
@@ -0,0 +1,25 @@
+(async function () {
+    document.querySelector('.movies').innerHTML = '<div class="col-md bg-warning">Loading...</div>';
+
+    let req = await fetch('https://api.b7web.com.br/cinema/');
+    let reqJson = await req.json();
+
+    mountMovie(reqJson);
+
+    function mountMovie (list) {
+        let html = '';
+
+        for(let i in list) {
+            html += 
+            `<div class="col-md-4 d-flex justify-content-center">
+                <figure class="figure bg-light border">
+                    <img src="${list[i].avatar}" class="figure-img img-fluid p-2" alt="...">
+                    <figcaption class="figure-caption text-center">${list[i].titulo}</figcaption>
+                </figure>
+            </div>`;
+        }
+
+        document.querySelector('.movies').innerHTML = html;
+    }
+})();
+
